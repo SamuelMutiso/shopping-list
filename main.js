fe1c398f3b584d6dd869dbec7200dc16300d6026
SamuelMutiso <muuosamuel335@gmail.com>
2026-04-06 10:13:37 +0300

    DOM

---
+++ main.js
@@ -0,0 +1,17 @@
+// DOM ELEMENTS
+// EMPTY SHOPPING LIST
+// FUNCTIONS TO DISPLAY SHOPPING LIST
+// BUTTONS
+// SAVE LOCAL STORAGE
+
+//DOM ELEMENTS
+const addBtn = document.getElementById ("addBtn");
+const clearbtn = document.getElementById ("clearbtn");
+const itemInput = document.getElementById ("itemInput ");
+const itemprice = document.getElementById ("itemprice");
+const list = document.getElementById ("shoppinglist");
+const totaldisplay = document.getElementById ("total");
+//shopping list array
+let shoppinglist[];
+
+
